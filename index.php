be2a5a9341d37db6a7f9a3b24463dcd597b37a16
Anastasiya Okisheva <nastiiazovyt@gmail.com>
2022-09-01 10:47:57 +0700

    - почистила и подготовила проект к работе

--- index.php
+++ index.php
@@ -2,10 +2,7 @@
 <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/blocks/meta/header.php' ?>
 
 <main>
-    <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/blocks/meta/hero.php' ?>
-    <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/blocks/meta/menu.php' ?>
-    <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/blocks/meta/info.php' ?>
-    <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/blocks/meta/events.php' ?>
+    <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/blocks/other/main.php' ?>
 </main>
 
 <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/blocks/meta/footer.php' ?>
--- src/js/pages/index.js
+++ src/js/pages/index.js
@@ -1,9 +1,3 @@
 document.addEventListener('DOMContentLoaded', function (event) {
-    require('/src/js/pages/menu-swiper')
-    require('/src/js/pages/header')
-    require('/src/js/pages/hero')
-    require('/src/js/pages/menu')
-    require('/src/js/pages/info')
-    require('/src/js/pages/events')
-    require('/src/js/pages/footer')
+    require('/src/js/other/main')
 })
